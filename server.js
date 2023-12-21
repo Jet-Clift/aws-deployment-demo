3ff6718c6cff24bfe4705cf65c6e0171f87aa7f1
jet-clift <jet.clift@gmail.com>
2023-12-21 12:17:48 -0800

    redo

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
 app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000, () => console.log("Up on 4000"))
+
+
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: '767502d0abd146449360ddc790aabaed',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+
+})
+rollbar.log('hello World!')
